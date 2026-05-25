6971d82f2b8de3c629df91d7040ffe4f2ade0092
KevinStiven04 <kevinmontoya0614@gmail.com>
2026-05-25 15:45:55 -0500

    Feat: Cambio de link

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-sistemas-de-reserva-del-hotel-en.onrender.com',
+  apiUrl: 'https://backend-sistemas-de-reserva-del-hotel-en-0gvu.onrender.com',
 };
